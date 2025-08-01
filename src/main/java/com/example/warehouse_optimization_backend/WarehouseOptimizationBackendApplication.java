f6a9f1490aee039e157d7ea52fa93d05213fc5e1
Najimuddin Shaikh <najimuddin@rootnode.in>
2025-08-01 23:22:36 +0530

    Files added

---
+++ src/main/java/com/example/warehouse_optimization_backend/WarehouseOptimizationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.warehouse_optimization_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseOptimizationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseOptimizationBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/warehouse_optimization_backend/WarehouseOptimizationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.warehouse_optimization_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseOptimizationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
